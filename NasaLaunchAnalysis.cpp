f434f466ddc5780f10f4e91354b982e80b79675c
Barbara Papadopoulou <barbarapapadopoulou@barbarasair2038.fandm.edu>
2026-09-23 22:59:26 -0400

    Added the try and catch so it does not print the entire csv file as before

--- NasaLaunchAnalysis.cpp
+++ NasaLaunchAnalysis.cpp
@@ -1,11 +1,28 @@
 #include <fstream> 
-#include <iostream>
-#include <string>
 //for fstream used this to figure out how to properly read a csv file: https://medium.com/@ryan_forrester_/reading-csv-files-in-c-how-to-guide-35030eb378ad 
+#include <iostream>
+#include <vector>
+#include <string>
+#include "TimeCode.h"
+#include <stdexcept> //use this for invalid argument
 
-//Did not know where to put this comment but i did read the comments/notes in the assignment file 
+//Did not know where to put this comment but i did read the comment 3/note 3 in the assignment file 
 
 using namespace std; 
+
+TimeCode parse_line(string line) {
+    size_t colonPos = line.find(':');
+
+    if (colonPos == string::npos) {
+        throw invalid_argument("No time found in line");
+    }//this is for when there is no time npos allows me to see if the parameter I am searchign for doesn't exist
+
+    unsigned int hr = stoi(line.substr(colonPos - 2, 2));
+    unsigned int min = stoi(line.substr(colonPos + 1, 2));
+
+    return TimeCode(hr, min, 0); // seconds not given in the file, use 0
+}
+
 
 vector<string> split(string s, char delimeter) {
     vector<string> pieces;
@@ -31,13 +48,23 @@ int main() {
         cout << "Could not open file!" << endl;
         return 1;
     }
-
+	
+	vector<TimeCode> times;
     string line;
-    while (getline(file, line)) {
-        cout << line << endl;
-    }
+    getline(file, line);
+    
+	while (getline(file, line)) {
+		try {
+			TimeCode tc = parse_line(line);
+			cout << tc.ToString() << endl;
+			times.push_back(tc);
+		} catch (const invalid_argument& e) {
+			// no time in this row, skip it and move to the next line
+		}
+	}
+	
+	file.close();
 
-    file.close();
     return 0;
 }
 	
